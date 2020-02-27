b34b2c908824ba37ce36cfb4b08c836aa7532277
Ben Stuart <benarthurstuart@hotmail.com>
2020-02-27 23:25:29 +0000

    refactored to pull jquery into its own file

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,13 +1,19 @@
 describe('CatAPI', function() {
-  it('contains The Cat Caller header', function() {
+  beforeEach(function(){
     cy.visit('localhost:3000');
+  });
 
+  it('contains The Cat Caller header', function() {
     cy.contains('The Cat Caller');
   });
 
   it('contains a button', function() {
-    cy.visit('localhost:3000');
-
     cy.get('#cat_button');
+  });
+
+  it('produces a kitten', function() {
+    cy.get('#cat_button').click();
+    cy.wait(3000) // delay for the api call to finish - 3 seconds
+    cy.get('#cat_image').should('have.attr', 'src')
   });
 });
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#cat_button").click(function(){
     $.ajax({
-      url: "https://api.thecatapi.com/v1/images/search?",
+      url: "https://api.thecatapi.com/v1/images/search",
       headers: { "x-api-key": "9c1793b9-97c3-488f-bb19-e8b94f281470" },
       type: "GET",
       dataType: "json",
